e729aea0a8e117d3530ce518769ca71937add5b2
Jach Yeh <jack59074@gmail.com>
2019-04-23 07:03:18 +0800

    backup

--- model.go
+++ model.go
@@ -7,27 +7,35 @@ import (
 
 type UserInfo struct {
 	gorm.Model
-	Username string   `gorm:"type:varchar(16);not null;unique_index" json:"username" binding:"required"`
-	Password string   `gorm:"type:varchar(128);not null;" json:"password" binding:"required"`
-	RoleID   uint     `sql:"type:int unsigned" gorm:"not null;" json:"role_id"`
-	Role     RoleInfo `gorm:"foreignkey:RoleID"`
+	Username string        `gorm:"type:varchar(16);not null;unique_index" json:"username" binding:"required"`
+	Password string        `gorm:"type:varchar(128);not null;" json:"password" binding:"required"`
+	RoleID   uint          `sql:"type:int unsigned" gorm:"not null;" json:"role_id"`
+	Role     RoleInfo      `gorm:"foreignkey:RoleID" json:"-"`
+	Projects []ProjectInfo `gorm:"many2many:user_projects;" json:"-"`
 }
 
 type RoleInfo struct {
 	gorm.Model
-	Name string `gorm:"type:varchar(16);not null;unique_index" json:"role_name"`
+	Name       string `gorm:"type:varchar(16);not null;unique_index" json:"role_name"`
+	Permission string `gorm:"type:varchar(255);" json:"permission"`
 }
 
 type ProjectInfo struct {
 	gorm.Model
-	Name string `gorm:"type:varchar(32);not null;unique_index" json:"project_name" binding:"required"`
+	Name  string     `gorm:"type:varchar(32);not null;unique_index" json:"project_name" binding:"required"`
+	Users []UserInfo `gorm:"many2many:user_projects;" json:"-"`
+}
+
+type UserProject struct {
+	UserID    uint `gorm:"not null;unique_index:idx_user_proj" json:"user_id" binding:"required"`
+	ProjectID uint `gorm:"not null;unique_index:idx_user_proj" json:"project_id" binding:"required"`
 }
 
 type ProjectEnv struct {
 	gorm.Model
-	ProjectID uint        `sql:"type:int unsigned" gorm:"not null;" json:"project_id" binding:"required"`
-	Project   ProjectInfo `gorm:"foreignkey:ProjectID"`
-	EnvKey    string      `gorm:"type:varchar(32);not null;unique_index" json:"env_key" binding:"required"`
+	ProjectID uint        `sql:"type:int unsigned" gorm:"not null;unique_index:idx_proj_env" json:"project_id" binding:"required"`
+	Project   ProjectInfo `gorm:"foreignkey:ProjectID" json:"-"`
+	EnvKey    string      `gorm:"type:varchar(32);not null;unique_index:idx_proj_env" json:"env_key" binding:"required"`
 	EnvValue  string      `gorm:"type:varchar(128);not null;" json:"env_value" binding:"required"`
 	IsSecret  bool        `gorm:"not null;" json:"is_secret" binding:"required"`
 }
@@ -51,11 +59,10 @@ func getDBInstance() *gorm.DB {
 func migration() {
 	db := getDBInstance()
 	// db.DropTable(&UserInfo{}, &RoleInfo{}, &ProjectInfo{}, &ProjectEnv{})
-	db.AutoMigrate(&UserInfo{}, &RoleInfo{}, &ProjectInfo{}, &ProjectEnv{})
+	db.AutoMigrate(&UserInfo{}, &RoleInfo{}, &ProjectInfo{}, &ProjectEnv{}, &UserProject{})
 }
 
 func seedData(dataList ...interface{}) {
-
 	db := getDBInstance()
 	for _, v := range dataList {
 		db.Create(v)
@@ -65,8 +72,8 @@ func seedData(dataList ...interface{}) {
 func seed() {
 	seedData(
 		&UserInfo{Username: "admin", Password: encrypt("admin"), RoleID: 1},
-		&RoleInfo{Name: "admin"},
-		&RoleInfo{Name: "user"},
-		&RoleInfo{Name: "guest"},
+		&RoleInfo{Name: "admin", Permission: `{"user":["create","read","update","delete"],"project":["create","read","update","delete"],"env":["create","read","update","delete"]}`},
+		&RoleInfo{Name: "leader", Permission: `{"user":[],"project":["read"],"env":["create","read","update","delete"]}`},
+		&RoleInfo{Name: "guest", Permission: `{"user":[],"project":["read"],"env":["read"]}`},
 	)
 }
--- route.go
+++ route.go
@@ -1,13 +1,23 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
+	"reflect"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 	"golang.org/x/crypto/bcrypt"
 )
 
 const hmacSampleSecret = "GYthbtJJ6tp3852JMEVmVHhDckdHHDsJ"
+
+type jsonPermission struct {
+	User    []string `json: "user"`
+	Project []string `json: "project"`
+	Env     []string `json: "env"`
+}
 
 func saveUser(user UserInfo) bool {
 	db := getDBInstance()
@@ -85,6 +95,77 @@ func authRequired() gin.HandlerFunc {
 	}
 }
 
+func contains(arr []string, element string) bool {
+	for _, v := range arr {
+		if element == v {
+			return true
+		}
+	}
+	return false
+}
+
+func verifyPermission(roleID, resource, action string) bool {
+	db := getDBInstance()
+
+	role := RoleInfo{}
+	check(db.Where("id =?", roleID).Find(&role).Error)
+	var j jsonPermission
+
+	err := json.Unmarshal([]byte(role.Permission), &j)
+	check(err)
+
+	v := reflect.ValueOf(&j).Elem().FieldByName(strings.Title(resource))
+	arr := v.Interface().([]string)
+	// fmt.Println(arr)
+
+	return contains(arr, action)
+}
+
+func isValid(resource, action string) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// Verify role
+		roleID, ok := c.Get("roleID")
+		if !ok {
+			c.AbortWithStatus(http.StatusUnauthorized)
+			return
+		}
+
+		roleStr := fmt.Sprint(roleID.(uint))
+		if verifyPermission(roleStr, resource, action) {
+			// if permission is valid, then next
+			c.Next()
+			return
+		}
+		// else return http 401
+		c.AbortWithStatus(http.StatusUnauthorized)
+		return
+	}
+}
+
+func getUsers() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		db := getDBInstance()
+		var users []UserInfo
+		db.Select("id, username, role_id").Find(&users)
+
+		c.JSON(200, gin.H{
+			"data": users,
+		})
+	}
+}
+
+func addUsers() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// db := getDBInstance()
+		var users []UserInfo
+		c.BindJSON(&users)
+
+		c.JSON(200, gin.H{
+			"data": users,
+		})
+	}
+}
+
 func getRouter() *gin.Engine {
 	r := gin.Default()
 
@@ -102,11 +183,20 @@ func getRouter() *gin.Engine {
 			})
 		})
 
-		// authorized.GET("/users")
+		authorized.GET("/users", isValid("user", "read"), getUsers())
+		authorized.POST("/users", isValid("user", "create"), addUsers()) // add user
+		// authorized.POST("/user/:user_id/project", isValid("project", "create"), addProjectsForUser()) // assign project for user
+
+		// authLeader := authorized.Group("leader")
+		// authLeader.Use(isLeader())
+		// {
+		// 	authLeader.GET("/user/:user_id/projects")
+		// 	authLeader.POST("/project/:project_id/env") // add env for project
+		// }
+
 		// authorized.POST("/user") // add user
 
 		// authorized.GET("/user/:user_id/projects")
-		// authorized.POST("/user/:user_id/project") // assign project for user
 
 		// authorized.GET("/project/:project_id/envs")
 		// authorized.POST("/project/:project_id/env") // add env for project
